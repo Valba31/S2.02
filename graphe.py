7a37c3c398ee69287f8328a41bb4b6e1634fd68a
Axel <axel.teulat@etu.iut-tlse3.fr>
2026-05-11 17:23:04 +0200

    séparation des fonctions

--- graphe.py
+++ graphe.py
@@ -1,6 +1,19 @@
 import numpy as np
 
-def graphe(n, p, a, b):
+def graphe(n, a, b):
+    T = np.random.randint(0, 2, (n, n))
+    T = T.astype('float64')
+    
+    for i in range(n):
+        for j in range(n):
+            if T[i][j] != 0:
+                T[i][j] = np.random.randint(a, b)
+            else:
+                T[i][j] = float('inf')
+
+    return T
+
+def graphe2(n, p, a, b):
     T = np.random.binomial(1, p, (n, n))
     T = T.astype('float64')
     
@@ -13,4 +26,4 @@ def graphe(n, p, a, b):
     
     return T
 
-print(graphe(10, 0.5, 0, 5))
+print(graphe(10, 0, 10))
